5865dd99f3e48b27a5cb8d58d93d381e3036dda8
DariaT22 <daria.tom22@mail.ru>
2023-03-06 11:28:31 +0300

    add: project

---
+++ script.js
@@ -0,0 +1,74 @@
+alert ('js runs');
+//ex1
+/*for (let i=1; i<=50; i++) {
+    console.log (i);
+}
+for (let i=35; i>=8; i--) {
+    console.log (i);
+}
+*/
+
+
+//ex2
+/*let i=89;
+while (i>=11) {
+    console.log (i);
+    i--;
+}
+*/
+
+
+//ex3
+/*a=100;
+let sum=0;
+for (let i=0; i<a; i++) {
+    sum +=i; 
+}
+console.log (sum);
+*/
+
+
+
+//ex4
+/*a=5;
+let sum=0;
+for (let i=0; i<a; i++) {
+    sum +=i; 
+    console.log (sum);
+}
+*/
+
+
+//ex5   
+/*for (i=8; i<=56; i++){
+    if (i%2==1) continue;
+    console.log(i);
+}
+
+
+let i=8;
+while (i<=56) {
+    i++;
+    if (i%2==0)
+    console.log (i);
+}
+*/
+
+
+//ex6 НЕ ЗНАЮ КАК РЕШИТЬ
+/*i=2;
+while (i<=10) {
+    j=2;
+    while (j<=10) {
+        console.log( i+'*'+ j+'='+(i*k));
+        j++;
+    }
+    console.log(i+'*'+ j+'='+(i*k));
+    i++;
+}
+*/
+ 
+
+
+
+//ex8
